63f4421eded5a084c2bd4483a8b2a3ecc81273c3
Thintalltom <adeyanjutomide@gmail.com>
2025-04-14 14:41:58 +0100

    added code to create S3 bucket

---
+++ bucketCreate.js
@@ -0,0 +1,43 @@
+import pkg from "aws-sdk";
+const { S3 } = pkg;
+import fs from "fs";
+const s3 = new S3();
+const BUCKET_NAME = "file-upload200";
+// Create a bucket if it doesn't exist
+const createBucket = (bucketName) => {
+  let bucketParams = { Bucket: bucketName };
+  s3.createBucket(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.Location);
+    }
+  });
+};
+
+//list all buckets
+const listBuckets = () => {
+  s3.listBuckets({}, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.Buckets);
+    }
+  });
+};
+
+//upload files in buckets
+const uploadFile = (bucketName, keyName, filePath) => {
+  const file = fs.readFileSync(filePath);
+  const uploadParams = { Bucket: bucketName, Key: keyName, Body: file };
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Upload Success", data.Location);
+    }
+  });
+};
+
+listBuckets();
+uploadFile(BUCKET_NAME, "user-image", '../file-upload/kgen.png');
---
+++ src/handlers/s3-json-logger.mjs
@@ -0,0 +1,49 @@
+import {
+  S3Client,
+  GetObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import { sdkStreamMixin } from "@aws-sdk/util-stream-node";
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+const s3 = new S3Client({});
+const sns = new SNSClient({});
+const SNS_ARN = "arn:aws:sns:us-east-1:529088262208:fileprocessedtopic";
+const processed_Bucket = "processedbkt224";
+
+
+export const s3JsonLoggerHandler = async (event, context) => {
+  try {
+    const record = event.Records[0];
+    const bucket = record.s3.bucket.name;
+    const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+
+    const { Body } = await s3.send(
+      new GetObjectCommand({ Bucket: bucket, Key: key })
+    );
+    const stream = sdkStreamMixin(Body);
+    const content = await stream.transformToString();
+    console.log("📦 File contents:", content);
+
+    await s3.send(
+      new CopyObjectCommand({
+        Bucket: processed_Bucket,
+        CopySource: `${bucket}/${key}`,
+        Key: key,
+      })
+    );
+
+    await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key }));
+
+    await sns.send(
+      new PublishCommand({
+        TopicArn: SNS_ARN,
+        Subject: "✅ File Processed",
+        Message: `File "${key}" has been processed and moved to "${PROCESSED_BUCKET}".`,
+      })
+    );
+    console.log("🎉 File processed successfully.");
+  } catch (error) {
+    console.error("❌ Error processing file:", error);
+  }
+};
